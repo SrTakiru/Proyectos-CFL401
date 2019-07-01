7db01ded6622f26748ecc667df351b4e501970be
Cyro Benjamin Lobaty <cyrolobaty@gmail.com>
2019-07-01 18:58:46 -0300

    Agregamos un System.out.println("...");
    No hice nada practicamente

--- Lobaty - Test - ArrayList/src/lobaty/test/arraylist/LobatyTestArrayList.java
+++ Lobaty - Test - ArrayList/src/lobaty/test/arraylist/LobatyTestArrayList.java
@@ -1,6 +1,7 @@
 package lobaty.test.arraylist;
 public class LobatyTestArrayList {
     public static void main(String[] args) {
+        System.out.println("Damn bruh this is hard");
     }
     
 }
