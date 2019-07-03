2f7a8a29066341ed56f98cf9964ff972232cada4
Programación <Programacin@DESKTOP-69L8MAA>
2019-07-03 18:23:04 -0300

    Comienzo una prueba o alpha de un programa simple con interfaz grafica

---
+++ Lobaty - Test - MultiClass & Interfaz/src/lobaty/test/multiclass/interfaz/LobatyTestMultiClassInterfaz.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lobaty.test.multiclass.interfaz;
+
+/**
+ *
+ * @author Programación
+ */
+public class LobatyTestMultiClassInterfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
