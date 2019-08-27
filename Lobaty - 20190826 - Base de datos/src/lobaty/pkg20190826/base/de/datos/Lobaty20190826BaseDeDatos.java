3446466ec855bd7501954a56222c3be87e34d0e6
Programación <Programacin@DESKTOP-69L8MAA>
2019-08-26 21:30:02 -0300

    Probamos las funciones

--- Lobaty - 20190826 - Base de datos/src/lobaty/pkg20190826/base/de/datos/Lobaty20190826BaseDeDatos.java
+++ Lobaty - 20190826 - Base de datos/src/lobaty/pkg20190826/base/de/datos/Lobaty20190826BaseDeDatos.java
@@ -8,13 +8,18 @@ public class Lobaty20190826BaseDeDatos {
         Connection conexion;
         conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cfl401?user=programador&password=cfl401");
         Statement consultor = conexion.createStatement();
-        ResultSet resultado = consultor.executeQuery("SELECT * FROM datosPersonales;");
+        ResultSet resultado = consultor.executeQuery("SELECT * FROM datosPersonales WHERE edad<50 ORDER BY edad DESC;");
         while (resultado.next()) {
             System.out.println(resultado.getInt("id"));
             System.out.println(resultado.getString("apellido"));
             System.out.println(resultado.getString("nombre"));
+            System.out.println(resultado.getString("fechaNac"));
+            System.out.println(resultado.getString("fechaFall"));
         }
         conexion.close();
     }
 }
+
+
+
 
