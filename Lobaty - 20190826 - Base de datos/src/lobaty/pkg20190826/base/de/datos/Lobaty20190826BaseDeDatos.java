5911b2c8cea70659a67299d75fea15feb01f10ca
Programación <Programacin@DESKTOP-69L8MAA>
2019-08-26 21:25:02 -0300

    Conectamos la base de datos, creamos el "consultor" y el "resultado"

--- Lobaty - 20190826 - Base de datos/src/lobaty/pkg20190826/base/de/datos/Lobaty20190826BaseDeDatos.java
+++ Lobaty - 20190826 - Base de datos/src/lobaty/pkg20190826/base/de/datos/Lobaty20190826BaseDeDatos.java
@@ -8,6 +8,13 @@ public class Lobaty20190826BaseDeDatos {
         Connection conexion;
         conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cfl401?user=programador&password=cfl401");
         Statement consultor = conexion.createStatement();
+        ResultSet resultado = consultor.executeQuery("SELECT * FROM datosPersonales;");
+        while (resultado.next()) {
+            System.out.println(resultado.getInt("id"));
+            System.out.println(resultado.getString("apellido"));
+            System.out.println(resultado.getString("nombre"));
+        }
+        conexion.close();
     }
 }
 
