7b9858b591fbdee74002523e59affb93b18117ae
Programación <Programacin@DESKTOP-69L8MAA>
2019-09-06 21:34:10 -0300

    Empezamos a poner las propiedades

--- GestionPersonal/src/gestionpersonal/GestionPersonal.java
+++ GestionPersonal/src/gestionpersonal/GestionPersonal.java
@@ -9,10 +9,18 @@ public class GestionPersonal {
     private static CargaUsuarios cargaUsuarios;
 
     public static void main(String[] args) throws SQLException {
+        menuPrincipal = new MenuPrincipal();
+        cargaUsuarios = new CargaUsuarios();
+        generarTablas = new GenerarTablas();
         Connection conexion;
         conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cfl401?user=programador&password=cfl401");
         Statement consultor = conexion.createStatement();
-        ResultSet resultado = consultor.executeQuery("");
+        // ResultSet resultado = consultor.executeQuery("");
+        menuPrincipal.setVisible(true);
+    }
+
+    public static void cambiarVentana(){
+        
     }
 }
 
